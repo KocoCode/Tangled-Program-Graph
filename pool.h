32ba93f62a6ede8877fcadc637fe19b2b177b928
Yuan-Yao Sung <yysung@cs.nctu.edu.tw>
2018-12-29 15:50:02 +0800

    initial commit

---
+++ pool.h
@@ -0,0 +1,70 @@
+#include <vector>
+#include <random>
+#include <unordered_map>
+#include <utility>
+
+using std::default_random_engine;
+using std::pair;
+using std::swap;
+using std::vector;
+using std::uniform_int_distribution;
+using std::unordered_map;
+
+class RandomizedSet {
+private:
+    vector<long long> v;
+    unordered_map<long long, long long> m;
+    default_random_engine gen;
+    uniform_int_distribution<long long> uniform;
+
+public:
+    void insert(long long val) {
+        m[val] = v.size();
+        v.push_back(val);
+    }
+
+    void remove(long long val) {
+        long long back = v.back();
+        swap(v[m[val]], v.back());
+        m[back] = m[val];
+        m.erase(val);
+        v.pop_back();
+    }
+
+    long long random() {
+        return v[uniform(gen, decltype(uniform)::param_type(0, v.size() - 1))];
+    }
+};
+
+template <class T>
+class Pool {
+private:
+    RandomizedSet rs;
+    long long count = 0;
+    vector<long long> gc;
+
+public:
+    unordered_map<long long, T> pool;
+    T create() {
+        rs.insert(count);
+        pool[count];
+        return count++;
+    }
+
+    pair<long long, T> random() {
+        long long id = rs.random();
+        return {id, pool[id]};
+    }
+
+    void remove(long long id) {
+        gc.push_back(id);
+        rs.remove(id);
+    }
+
+    void maintain() {
+        for (auto id : gc) {
+            pool.erase(id);
+        }
+        gc.clear();
+    }
+};
---
+++ pool_test.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cassert>
+#include "pool.h"
+
+using namespace std;
+
+int main() {
+    Pool<int> pool;
+    for (int i = 0; i < 100; ++i) {
+        pool.pool[pool.create()] = i;
+    }
+    for (int i = 0; i < 1000; ++i) {
+        int r = pool.random().second;
+        assert(r >= 0 and r < 100);
+    }
+    assert(pool.pool.size() == 100);
+    for (int id = 0; id < 100; ++id) {
+        if (id == 50) continue;
+        pool.remove(id);
+    }
+    for (int i = 0; i < 1000; ++i) {
+        int r = pool.random().second;
+        assert(pool.pool[r] == 50);
+    }
+    for (int i = 0; i < 50; ++i) {
+        pool.pool[pool.create()] = i;
+    }
+    for (int i = 0; i < 1000; ++i) {
+        int r = pool.random().second;
+        assert(r >= 0 and r <= 50);
+    }
+    pool.maintain();
+    assert(pool.pool.size() == 51);
+}
