f0ee6df05dd6403854dda34a38435e9481544830
Yuan-Yao Sung <sungyuanyao@gmail.com>
2019-01-03 21:01:27 +0800

    SFINAE for root team maintenance

--- pool.h
+++ pool.h
@@ -5,6 +5,8 @@
 #include <utility>
 
 using std::default_random_engine;
+using std::enable_if_t;
+using std::forward;
 using std::pair;
 using std::swap;
 using std::vector;
@@ -20,11 +22,13 @@ private:
 
 public:
     void insert(long long val) {
+        if (m.count(val)) return;
         m[val] = v.size();
         v.push_back(val);
     }
 
     void remove(long long val) {
+        if (m.count(val) == 0) return;
         long long back = v.back();
         swap(v[m[val]], v.back());
         m[back] = m[val];
@@ -37,10 +41,27 @@ public:
     }
 };
 
+template<class, class=void>
+struct is_isRoot_defined
+    : std::false_type {};
+
+template<class T>
+struct is_isRoot_defined<T, decltype(void(std::declval<T>().isRoot()))>
+    : std::true_type {};
+
+template<class, class=void>
+struct is_setRoot_defined
+    : std::false_type {};
+
+template<class T>
+struct is_setRoot_defined<T, decltype(void(std::declval<T>().setRoot(false)))>
+    : std::true_type {};
+
 template <class T>
 class Pool {
 private:
     RandomizedSet rs;
+    typename std::conditional<is_isRoot_defined<T>::value, RandomizedSet, nullptr_t>::type root_rs;
     long long count = 0;
     unordered_map<long long, T> pool;
     vector<long long> gc;
@@ -50,15 +71,39 @@ public:
         return pool.size();
     }
 
-    void insert(T& val) {
+    template <class T_>
+    std::enable_if_t<is_isRoot_defined<T_>::value> insert(T_&& val) {
+        if (val.isRoot()) {
+            root_rs.insert(count);
+        }
         rs.insert(count);
         val.setId(count);
-        pool.insert({count, val});
+        pool.insert({count, forward<T_>(val)});
         count++;
+    }
+
+    template <class T_>
+    std::enable_if_t<!is_isRoot_defined<T_>::value> insert(T_&& val) {
+        rs.insert(count);
+        val.setId(count);
+        pool.insert({count, forward<T_>(val)});
+        count++;
+    }
+
+    template <class T_ = T>
+    std::enable_if_t<is_setRoot_defined<T_>::value> addRootTeam(long long id) {
+        pool[id].setRoot(true);
+        root_rs.insert(id);
     }
 
     pair<long long, T&> random() {
         long long id = rs.random();
+        return {id, pool[id]};
+    }
+
+    template <class T_ = T>
+    std::enable_if_t<is_isRoot_defined<T_>::value, pair<long long, T&>> randomRootTeam() {
+        long long id = root_rs.random();
         return {id, pool[id]};
     }
 
@@ -67,8 +112,17 @@ public:
     }
 
     void remove(long long id) {
+        if constexpr(is_isRoot_defined<T>::value) {
+            root_rs.remove(id);
+        }
         gc.push_back(id);
         rs.remove(id);
+    }
+
+    template <class T_ = T>
+    std::enable_if_t<is_setRoot_defined<T_>::value> removeRootTeam(long long id) {
+        pool[id].setRoot(false);
+        root_rs.remove(id);
     }
 
     void maintain() {
--- pool_test.cpp
+++ pool_test.cpp
@@ -4,13 +4,49 @@
 
 using namespace std;
 
+class Foo {
+private:
+    int val, id;
+public:
+    Foo() {}
+    Foo(int v): val(v) {}
+    int getVal() const {
+        return val;
+    }
+    void setId(int v) {
+        id = v;
+    }
+};
+
+class HasRoot {
+private:
+    int val;
+    bool root;
+    int id;
+public:
+    HasRoot() {}
+    HasRoot(int v, bool r): val(v), root(r) {}
+    bool isRoot() const {
+        return root;
+    }
+    int getVal() const {
+        return val;
+    }
+    void setRoot(bool r) {
+        root = r;
+    }
+    void setId(int v) {
+        id = v;
+    }
+};
+
 int main() {
-    Pool<int> pool;
+    Pool<Foo> pool;
     for (int i = 0; i < 100; ++i) {
-        pool.insert(i);
+        pool.insert(Foo(i));
     }
     for (int i = 0; i < 1000; ++i) {
-        int r = pool.random().second;
+        auto r = pool.random().second.getVal();
         assert(r >= 0 and r < 100);
     }
     assert(pool.size() == 100);
@@ -19,16 +55,43 @@ int main() {
         pool.remove(id);
     }
     for (int i = 0; i < 1000; ++i) {
-        int r = pool.random().second;
-        assert(pool.get(r) == 50);
+        auto r = pool.random().second.getVal();
+        assert(pool.get(r).getVal() == 50);
     }
     for (int i = 0; i < 50; ++i) {
-        pool.insert(i);
+        pool.insert(Foo(i));
     }
     for (int i = 0; i < 1000; ++i) {
-        int r = pool.random().second;
+        auto r = pool.random().second.getVal();
         assert(r >= 0 and r <= 50);
     }
     pool.maintain();
     assert(pool.size() == 51);
+    for (int i = 0; i < 51; ++i) {
+        auto r = pool.random().first;
+        pool.remove(r);
+    }
+    pool.maintain();
+    assert(pool.size() == 0);
+
+    Pool<HasRoot> root_pool;
+    root_pool.insert(HasRoot(0, true));
+    for (int i = 1; i < 10; ++i) {
+        auto tmp = HasRoot(i, false);
+        root_pool.insert(tmp);
+    }
+    for (int i = 0; i < 1000; ++i) {
+        int r = root_pool.random().second.getVal();
+        assert(r >= 0 and r <= 9);
+    }
+    for (int i = 0; i < 1000; ++i) {
+        int r = root_pool.randomRootTeam().second.getVal();
+        assert(r == 0);
+    }
+    root_pool.addRootTeam(9);
+    root_pool.removeRootTeam(0);
+    for (int i = 0; i < 1000; ++i) {
+        int r = root_pool.randomRootTeam().second.getVal();
+        assert(r == 9);
+    }
 }
